49d10f6af1829b96d725b1fe33fec20c6a0718a7
Vincent Lee <tienchil@hotmail.com>
2017-09-17 00:28:54 -0400

    Tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -54,14 +54,14 @@ class FG_eval {
 
     // Minimize the use of actuators.
     for (int t = 0; t < N - 1; t++) {
-      fg[0] += 70000.0 * CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 35000.0 * CppAD::pow(vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
       fg[0] += 2000.0 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-      fg[0] += 1000.0 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 500.0 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
     fg[1 + x_start] = vars[x_start];
