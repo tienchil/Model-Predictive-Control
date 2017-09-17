cea17d08969edd1c389fcc3f9c89510bcabfda63
Vincent Lee <tienchil@hotmail.com>
2017-09-16 23:37:56 -0400

    Tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -7,7 +7,7 @@ using CppAD::AD;
 
 // TODO: Set the timestep length and duration
 size_t N = 10;
-double dt = 0.1;
+double dt = 0.05;
 double ref_v = 40.0;
 
 // This value assumes the model presented in the classroom is used.
@@ -61,7 +61,7 @@ class FG_eval {
     // Minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
       fg[0] += 2000.0 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-      fg[0] += 1000.0 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 100.0 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
     fg[1 + x_start] = vars[x_start];
