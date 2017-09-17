e4b6c2ec38be1197371bf1d0a1d7fe82fe042b55
Vincent Lee <tienchil@hotmail.com>
2017-09-17 00:35:30 -0400

    Tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -48,7 +48,7 @@ class FG_eval {
     // The part of the cost based on the reference state.
     for (int t = 0; t < N; t++) {
       fg[0] += 10.0 * CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 4.0 * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 40.0 * CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
